63805ee4f427182ce5ff286a1a7631304aa01293
Le Luong Minh Hieu <hieullm@sdtech.vn>
2024-07-18 16:31:57 +0700

    update code

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("The DOM is fully loaded.");
+  // Add more JavaScript code as needed
+});
---
+++ js/static.js
@@ -0,0 +1,26 @@
+export const links = [
+  {
+    name: "Home",
+    path: "#",
+  },
+  {
+    name: "Pages",
+    path: "#",
+  },
+  {
+    name: "Features",
+    path: "#",
+  },
+  {
+    name: "Extensions",
+    path: "#",
+  },
+  {
+    name: "Tutorials",
+    path: "#",
+  },
+  {
+    name: "Contact us",
+    path: "#",
+  },
+];
