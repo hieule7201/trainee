fc1c781281876fdd1af592a2943bb1e2885198be
Le Luong Minh Hieu <hieullm@sdtech.vn>
2024-07-19 09:40:30 +0700

    done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  console.log("The DOM is fully loaded.");
-  // Add more JavaScript code as needed
+  const toggleButton = document.getElementById("navbar-toggle");
+  const navbarContent = document.getElementById("navbar");
+  const navbarIcon = document.getElementById("navbar-icon");
+  const headerBox = document.querySelector(".header-box");
+
+  const openIconSrc = "./assets/icon/navbar.svg"; // Replace with the path to your open icon image
+  const closeIconSrc = "./assets/icon/close.svg"; // Replace with the path to your close icon image
+
+  toggleButton.addEventListener("click", function () {
+    navbarContent.classList.toggle("close");
+    const isOpen = navbarContent.classList.toggle("open");
+    headerBox.classList.toggle("hard-bg");
+    navbarIcon.src = isOpen ? closeIconSrc : openIconSrc;
+  });
 });
